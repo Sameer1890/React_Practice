9450fb662bd2c7776e4c467c425c494021403fb6
Sameer_shams <sumairshams1890@gmail.com>
2024-11-24 16:06:04 +0500

    first_commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  let title ="Title";
+  return (
+<div className="expense" >
+<div className="">Date And Time</div>
+<div className=".expense-description">
+  <h1>Title</h1></div>
+<div className="expense-price">Amount</div>
+</div>
+  );
+}
+
+export default App;
